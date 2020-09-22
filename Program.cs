c12fb8cebc5e44eefaa986e17910a46ff5b8ff6e
odinokun <odinokun@gmail.com>
2020-09-22 13:59:57 +0300

    did first & second triangle

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //begin first triangle
+            for (int i = 1; i < 5; i++)
+            {
+                for (int k = 0; k < i; k++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine("");
+            }
+            Console.WriteLine("==================");
+            //begin first triangle
+            for (int i = 4; i > 0; i--)
+            {
+                for (int k = 0; k < i; k++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine("");
+            }
+        }
+    }
+}
